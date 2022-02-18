8fcb060ac354dca77b71c0eb72a051db4633226a
WilliamAfil <will2b@gmail.com>
2022-02-18 15:18:34 -0800

    Add features

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,11 +1,17 @@
 import clsx from "clsx";
+import { Mock } from "../mock/mock";
 
+interface NewMock extends Mock {
+  id: number;
+}
 interface Props {
   isOpen: boolean;
   onToggle: () => void;
+  selectedItem: NewMock | null | undefined;
+  total: number;
 }
 
-const Drawer = ({ isOpen, onToggle }: Props) => {
+const Drawer = ({ isOpen, onToggle, selectedItem, total }: Props) => {
   return (
     <aside className={clsx("relative bg-slate-100")}>
       <div
@@ -18,15 +24,35 @@ const Drawer = ({ isOpen, onToggle }: Props) => {
       >
         x
       </div>
-      <h3
+
+      <div
         className={clsx(
-          "transition-all",
-          "overflow-hidden",
+          "relative",
+          "transition-all delay-500",
           !isOpen ? "w-0" : "w-[200px]",
         )}
       >
-        drawer
-      </h3>
+        <div
+          className={clsx(
+            "mt-2 p-2 transition-opacity duration-500 ease-out",
+            !isOpen ? " opacity-0" : " opacity-100 delay-1000",
+          )}
+        >
+          {/* <div className="absolute top-0 right-0 w-full h-full bg-green-100"> */}
+          {/* <div className=" transition-opacity"> */}
+          {!selectedItem && <p className="mt-2">目前資料量：{total}</p>}
+          {selectedItem && (
+            <div>
+              <h2>{selectedItem.name}</h2>
+              <div className="flex flex-col justify-center space-y-2">
+                {selectedItem.items.map((pic) => (
+                  <img src={pic} alt={selectedItem.name} />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </aside>
   );
 };
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,49 +3,110 @@ import { useState } from "react";
 import { Mock } from "../mock/mock";
 import Drawer from "./Drawer";
 
+// TODO:
+// [X] 1. 加入 checkbox
+// [X] 1. disable checkbox if item.status is false
+// [X] 1. if checkbox is checked, bg of sibling element is altered
+// [X] 2. 加入 click event, 紀錄選取 id
+// [X] 2. 加入 double click event & open drawer
+// [X] 3. 如果 !item.status checkbox disabled
+// [X] 4. filter selected (id), return selected object
+// [X] 5. if !selected, show mockData.length
+
 interface Props {
   mockData: Mock[];
 }
 
 const MainContent = ({ mockData }: Props) => {
+  const newMockData = mockData.map((item, idx) => {
+    return { id: idx, ...item };
+  });
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  console.log("mockData :>> ", mockData);
+  const [selected, setSelected] = useState<string | null>(null);
 
   const toggleDrawerHandler = () => {
     setIsOpen((prev) => !prev);
   };
 
+  const selectHandler = (e: React.MouseEvent<HTMLInputElement>) => {
+    setSelected((e.target as Element).id); // SOLVE: Property 'id' does not exist on type 'EventTarget'.
+  };
+
+  const onDoubleClickHandler = (e: React.MouseEvent<HTMLLabelElement>) => {
+    console.log("點擊兩次: ", e.target);
+
+    if (!selected) return;
+    if (!isOpen) setIsOpen(true);
+  };
+
+  const selectedItem = selected
+    ? newMockData.find((item) => item.id === Number(selected))
+    : null;
+
   return (
     <div className="flex">
       <section className={clsx("flex-1", "mr-10 px-4 py-6")}>
         <h2>Main Content</h2>
-        <table className="w-full border">
-          <thead>
-            <tr>
-              <th className="py-2 w-24 border border-gray-700">STATUS</th>
-              <th className="py-2 border border-gray-700">NAME</th>
-            </tr>
-          </thead>
-          <tbody>
-            {mockData.map((item) => (
-              <tr>
-                <td className="py-2.5 text-center border border-gray-700">
+
+        <div>
+          {/* THEAD */}
+          <section className="w-full border flex">
+            <div className="w-24 px-4 py-2 border-r">STATUS</div>
+            <div className="px-4 py-2 flex-1">NAME</div>
+          </section>
+          {/* TBODY */}
+          {newMockData.map((item) => (
+            <div key={item.id}>
+              <label
+                htmlFor={item.id.toString()}
+                onDoubleClick={item.status ? onDoubleClickHandler : () => {}}
+              >
+                <input
+                  name="users"
+                  id={item.id.toString()}
+                  title="users"
+                  type="radio"
+                  className="sr-only peer"
+                  disabled={!item.status}
+                  onClick={selectHandler}
+                />
+                <section
+                  className={clsx(
+                    "w-full border-l border-r border-b flex",
+                    "peer-checked:bg-gray-100",
+                  )}
+                >
                   <div
-                    className={clsx(
-                      "inline-block w-4 h-4 rounded-full",
-                      item.status ? "bg-green-300" : "bg-gray-300",
-                    )}
-                  />
-                </td>
-                <td className="px-4 py-2 border border-gray-700">
-                  {item.name}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                    data-id={item.id.toString()}
+                    className="w-24 px-4 py-1 border-r text-center"
+                  >
+                    <div
+                      className={clsx(
+                        "inline-block w-4 h-4 rounded-full",
+                        item.status ? "bg-green-300" : "bg-gray-300",
+                      )}
+                    />
+                  </div>
+                  <div
+                    data-id={item.id.toString()}
+                    className="px-4 py-1 flex-1"
+                  >
+                    {item.name}
+                  </div>
+                </section>
+              </label>
+            </div>
+          ))}
+        </div>
       </section>
-      <Drawer isOpen={isOpen} onToggle={toggleDrawerHandler} />
+
+      <Drawer
+        isOpen={isOpen}
+        onToggle={toggleDrawerHandler}
+        selectedItem={selectedItem}
+        total={newMockData.length}
+      />
     </div>
   );
 };
